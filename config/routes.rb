b79ebd1162162decef3bfd161aa88c78860d9f46
tomoya1600 <on100.ogata.tomoya@gmail.com>
2021-05-12 16:02:11 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, onle: [:new, :create]
+  resources :rooms, onle: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
